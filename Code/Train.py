fb067193b98265988b24c379977426ed59cbd4fe
Abdul Aziz Ar Rasyid <125854046+azrsyd@users.noreply.github.com>
2025-12-02 09:42:26 +0700

    Update Train.py

--- Code/Train.py
+++ Code/Train.py
@@ -19,7 +19,7 @@ from model import VapSwin2SR
 #---------------------------------------------------------------------------------------------------
 # Configuration Loader
 #---------------------------------------------------------------------------------------------------
-def load_config(config_path="config.yaml"):
+def load_config(config_path="config.yml"):
     with open(config_path, "r") as f:
         config = yaml.safe_load(f)
     return config
